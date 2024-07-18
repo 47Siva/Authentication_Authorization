b14103ad6cbaf1b59838f1fd1ee7c1a4c15bb5c1
Siva.B <152258638+49Siva@users.noreply.github.com>
2024-07-18 16:39:49 +0530

    Product service

--- src/main/java/com/app/Authentication/Authorization/service/UserService.java
+++ src/main/java/com/app/Authentication/Authorization/service/UserService.java
@@ -18,13 +18,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.app.Authentication.Authorization.advice.SignatureException;
+import com.app.Authentication.Authorization.dto.CustomerAndProductDto;
+import com.app.Authentication.Authorization.dto.CustomerProductDto;
 import com.app.Authentication.Authorization.entity.Customer;
+import com.app.Authentication.Authorization.entity.CustomerProduct;
+import com.app.Authentication.Authorization.entity.Product;
 import com.app.Authentication.Authorization.entity.User;
 import com.app.Authentication.Authorization.enumeration.Role;
 import com.app.Authentication.Authorization.enumeration.Status;
 import com.app.Authentication.Authorization.repository.CustomerRepository;
+import com.app.Authentication.Authorization.repository.ProductRepository;
 import com.app.Authentication.Authorization.repository.UserRepository;
 import com.app.Authentication.Authorization.response.CustomerResponse;
+import com.app.Authentication.Authorization.response.InvoiceResponse;
 import com.app.Authentication.Authorization.response.MessageService;
 import com.app.Authentication.Authorization.response.UserMapper;
 import com.app.Authentication.Authorization.response.UserResponse;
@@ -41,6 +47,7 @@ public class UserService implements UserDetailsService {
 	private final UserRepository userRepository;
 	private final CustomerRepository customerRepository;
 	private final JwtService jwtService;
+	private final ProductRepository productRepository;
 	private @NonNull MessageService messagePropertySource;
 
 	public User createUser(User user) {
@@ -225,6 +232,59 @@ public class UserService implements UserDetailsService {
 			response.put("Status", HttpStatus.OK);
 			response.put("StatusCode", HttpStatus.OK.toString());
 			return ResponseEntity.ok(response);
+		}
+	}
+
+	public ResponseEntity<?> getCustomer(UUID id) {
+		Optional<Customer> customer = customerRepository.findById(id);
+		Map<String, Object> response = new HashMap<>();
+		if (customer.isPresent()) {
+			Customer customer2 = customer.get();
+
+			CustomerAndProductDto customerAndProductDto = CustomerAndProductDto.builder().id(customer2.getId())
+					.address(customer2.getAddress()).customerName(customer2.getCustomerName()).date(customer2.getDate())
+					.email(customer2.getEmail()).gender(customer2.getGender()).mobileNo(customer2.getMobileNo())
+					.customerProducts(null).build();
+
+			double totalProductAmount = 0d;
+			double singleProductAmount = 0d;
+			int availableQuantity = 0;
+
+			ArrayList<CustomerProductDto> dtolist = new ArrayList<>();
+			List<CustomerProduct> customerProduct = customer2.getCustomerProducet();
+			for (CustomerProduct obj : customerProduct) {
+				CustomerProductDto customerProductDto = CustomerProductDto.builder().id(obj.getId())
+						.price(obj.getPrice()).productName(obj.getProductName()).quantity(obj.getQuantity())
+						.totalAmount(obj.getTotalAmount()).build();
+				Optional<Product> product1 = productRepository.findByName(obj.getProductName());
+				Product productobj = product1.get();
+				singleProductAmount = obj.getQuantity() * productobj.getPrice();
+				totalProductAmount += singleProductAmount;
+				dtolist.add(customerProductDto);
+			}
+			customerAndProductDto.setCustomerProducts(dtolist);
+
+			String Gststr = "5%";
+			String shopDiscountStr = "2%";
+			double shopDiscount = Double.parseDouble(shopDiscountStr.replace("%", "")) / 100.0;
+			double Gst = Double.parseDouble(Gststr.replace("%", "")) / 100.0;
+			double GstAmount = Gst * totalProductAmount;
+			double discountAmount = shopDiscount * totalProductAmount;
+			double grandTotal = totalProductAmount + GstAmount - discountAmount;
+
+			InvoiceResponse invoiceResponse = InvoiceResponse.builder().customer(customerAndProductDto).gst(Gststr)
+					.shopDiscount(shopDiscountStr).gstAmount(GstAmount).discountAmount(discountAmount)
+					.grandTotalAmount(grandTotal).totalprouctsAmount(totalProductAmount).build();
+
+			response.put("Data", invoiceResponse);
+			response.put("Status", HttpStatus.OK);
+			response.put("StatusCode", HttpStatus.OK.toString());
+			return ResponseEntity.ok(response);
+		} else {
+			response.put("Error", messagePropertySource.messageResponse("Customer.notfound"));
+			response.put("Status", HttpStatus.NO_CONTENT);
+			response.put("StatusCode", HttpStatus.NO_CONTENT.toString());
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
 		}
 	}
 
--- src/main/java/com/app/Authentication/Authorization/validator/CustomerValidator.java
+++ src/main/java/com/app/Authentication/Authorization/validator/CustomerValidator.java
@@ -1,6 +1,7 @@
 package com.app.Authentication.Authorization.validator;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,20 +11,12 @@ import javax.security.auth.Subject;
 
 import org.springframework.stereotype.Service;
 
-import com.app.Authentication.Authorization.Deserializer.CustomerProductConverter;
 import com.app.Authentication.Authorization.advice.ObjectInvalidException;
 import com.app.Authentication.Authorization.entity.Customer;
-import com.app.Authentication.Authorization.entity.CustomerProduct;
-import com.app.Authentication.Authorization.entity.User;
 import com.app.Authentication.Authorization.enumeration.RequestType;
-import com.app.Authentication.Authorization.repository.CustomerRepository;
 import com.app.Authentication.Authorization.request.BuyProductRequest;
-import com.app.Authentication.Authorization.request.UserRegisterRequest;
 import com.app.Authentication.Authorization.response.MessageService;
-import com.app.Authentication.Authorization.security.JwtService;
 import com.app.Authentication.Authorization.service.CustomerServicee;
-import com.app.Authentication.Authorization.service.InvoiceService;
-import com.app.Authentication.Authorization.service.UserService;
 import com.app.Authentication.Authorization.util.ValidationUtil;
 
 import lombok.AllArgsConstructor;
@@ -31,20 +24,20 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class CustomerValidator {
-	
+
 	MessageService messageService;
 	private final CustomerServicee customerService;
-	
+
 	List<String> errors = null;
 	List<String> errorsObj = null;
 	Optional<Subject> subject = null;
-	
+
 	public ValidationResult validate(RequestType requestType, BuyProductRequest request) {
-		
+
 		errors = new ArrayList<>();
 		ValidationResult result = new ValidationResult();
 		Customer customer = null;
-		
+
 		if (ValidationUtil.isNullOrEmpty(request.getCustomerName())) {
 			errors.add(messageService.messageResponse("register.user.name.required"));
 		} else {
@@ -53,7 +46,7 @@ public class CustomerValidator {
 				errors.add(messageService.messageResponse("register.user.name.invalid"));
 			}
 		}
-		
+
 		if (ValidationUtil.isNullOrEmpty(ValidationUtil.getFormattedString(request.getEmail()))) {
 			errors.add(messageService.messageResponse("register.email.required"));
 		} else {
@@ -62,13 +55,12 @@ public class CustomerValidator {
 				errors.add(messageService.messageResponse("register.email.invalid"));
 			}
 		}
-		
+
 		Optional<Customer> userDuplicateMailObj = customerService.findByDuplicateEmail(request.getEmail());
 		if (userDuplicateMailObj.isPresent()) {
 			errors.add(messageService.messageResponse("user.register.email.duplicate"));
 		}
 
-		
 		if (ValidationUtil.isNullOrEmpty(request.getMobileNo())) {
 			errors.add(messageService.messageResponse("register.mobile.required"));
 		} else {
@@ -77,7 +69,7 @@ public class CustomerValidator {
 				errors.add(messageService.messageResponse("register.mobile.invalid"));
 			}
 		}
-		
+
 		if (ValidationUtil.isNullOrEmpty(request.getAddress())) {
 			errors.add(messageService.messageResponse("register.address.required"));
 		} else {
@@ -86,32 +78,40 @@ public class CustomerValidator {
 				errors.add(messageService.messageResponse("register.address.invalid"));
 			}
 		}
-		
+
 		Optional<Customer> userExist = customerService.getMobileNos(request.getMobileNo());
 		if (userExist.isPresent()) {
 			String[] params = new String[] { request.getMobileNo() };
 			errors.add(messageService.messageResponse("register.mobileno.exist", params));
 		}
-		
-		if(ValidationUtil.isGenderrequired(request.getGender())) {
+
+		if (ValidationUtil.isGenderrequired(request.getGender())) {
 			errors.add(messageService.messageResponse("register.gender.required"));
-		}else {
+		} else {
 			request.setGender(ValidationUtil.getFormattedGender(request.getGender()));
-			if(!ValidationUtil.isGenderValid(request.getGender())) {
+			if (!ValidationUtil.isGenderValid(request.getGender())) {
 				errors.add(messageService.messageResponse("register.gender.invalid"));
 			}
 		}
-		
+
+		if (request.getDate() != null) {
+			request.setDate(request.getDate());
+		} else {
+	        LocalDate date = LocalDate.now();
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+			request.setDate(date.format(formatter));
+		}
+
 		if (errors.size() > 0) {
 			String errorMessage = errors.stream().map(a -> String.valueOf(a)).collect(Collectors.joining(", "));
 			throw new ObjectInvalidException(errorMessage);
 		}
-		
+
 //		 List<CustomerProduct> customerProducts = CustomerProductConverter.convert(request.getCustomerProduct());
 
 		customer = Customer.builder().email(request.getEmail()).address(request.getAddress())
-				.customerName(request.getCustomerName()).date(request.getDate())
-				.mobileNo(request.getMobileNo()).gender(request.getGender()).build();
+				.customerName(request.getCustomerName()).date(request.getDate()).mobileNo(request.getMobileNo())
+				.gender(request.getGender()).build();
 		result.setObject(customer);
 		return result;
 	}
